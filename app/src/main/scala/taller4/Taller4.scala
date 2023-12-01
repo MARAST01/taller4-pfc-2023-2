971f45cb4fef141bc83906ef6182c6826654b48e
MARAST01 <marlon.astudillo@correounivalle.edu.co>
2023-12-01 11:11:52 -0500

    1.0

--- app/src/main/scala/taller4/Taller4.scala
+++ app/src/main/scala/taller4/Taller4.scala
@@ -12,6 +12,22 @@ import org.scalameter.Warmer
 object Taller4{
 
   def saludo() = "Taller 4 2023-II"
+  type Matriz = Vector[Vector[Int]]
+  def prodPunto(v1:Vector[Int],v2:Vector[Int]):Int = {
+    (v1 zip v2).map({case (i,j)=>i*j}).sum
+  }
+  def transpuesta (m:Matriz):Matriz = {
+    val l=m.length
+    Vector.tabulate(l,l)((i,j)=>m(j)(i))
+  }
+  def mulMatriz(m1:Matriz,m2:Matriz):Matriz = {
+    val m2t=transpuesta(m2)
+    Vector.tabulate(m1.length,m2t.length)((i,j)=>prodPunto(m1(i),m2t(j)))
+  }
+  // usando las abstracciones de parallel o task
+  def multMatrizPar (m1:Matriz,m2:Matriz):Matriz = {
+
+  }
 
   def main(args: Array[String]): Unit = {
     println(saludo())
